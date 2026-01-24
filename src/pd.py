e7c47c66a54efd07cf516eddb596938f6a2b3f16
leonkae <leon29kae@gmail.com>
2026-01-24 18:16:03 +0300

    pd file modified

---
+++ src/pd.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+numbers = pd.Series([1, 2, 3, 4, 5])
+print(numbers)
+
+ages = pd.Series([12, 13, 66], index=['Alice', 'Bob', 'Charlie'])
+print(ages)
+print("Alice's age:", ages['Alice'])
+
+# Creating a DataFrame
+people_data = {
+    'Name': ['John', 'Anna', 'Peter', 'Linda'],
+    'Age': [28, 24, 35, 32],
+    'City': ['New York', 'Paris', 'Berlin', 'London']
+}
+
+people_df = pd.DataFrame(people_data)
+print(people_df)
